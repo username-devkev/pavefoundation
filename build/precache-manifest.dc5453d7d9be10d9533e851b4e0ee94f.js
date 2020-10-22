ff74eb95aab60d3ddbdc4955d8d69e2b6510c40f
Kevin <Kevin@Kevins-MacBook-Pro.local>
2020-10-22 17:22:55 -0400

    netlify deploy

---
+++ build/precache-manifest.dc5453d7d9be10d9533e851b4e0ee94f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c128c3d6f74dc5599a1d361f18726187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0dcbfa1549847e2c0b4",
+    "url": "/static/css/2.d58c7b28.chunk.css"
+  },
+  {
+    "revision": "e0dcbfa1549847e2c0b4",
+    "url": "/static/js/2.4961c4b4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4961c4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62cc26be833ec90bb0a7",
+    "url": "/static/js/main.dcbfa353.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
