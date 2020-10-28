cfb01c3655f28bf6360dbee4bcea63471f1cfdd0
Kevin <Kevin@Kevins-MacBook-Pro.local>
2020-10-28 13:10:26 -0400

    Pave Foundation Form Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,155 +1,151 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import NewsletterForm from "../components/NewsletterForm";
 class Footer extends Component {
-    constructor(){
-        super()
-        this.state = {
-          scrollBtn: false
-        }
+  constructor() {
+    super();
+    this.state = {
+      scrollBtn: false,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    if (window.scrollY > 100) {
+      this.setState({
+        scrollBtn: true,
+      });
+    } else if (window.scrollY < 100) {
+      this.setState({
+        scrollBtn: false,
+      });
     }
+  };
 
-    componentDidMount(){
-        window.addEventListener('scroll', this.handleScroll);
-    }
+  scrollTop = () => {
+    window.scrollTo(0, 0);
+  };
 
-    handleScroll = () => {
-
-      if (window.scrollY > 100) {
-        this.setState({
-            scrollBtn: true
-        });
-      } else if (window.scrollY < 100) {
-        this.setState({
-            scrollBtn: false
-        });
-      }
-
-    }
-
-    scrollTop = () => {
-        window.scrollTo(0, 0);
-    }
-
-    render() {
-        return (
-            <div>
-                <section className="footer-area">
-                    <div className="newsletter-area">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-lg-6 mx-auto text-center">
-                                    <div className="section-heading footer-heading">
-                                        <div className="section-icon">
-                                            <img src="/images/section-icon.png" alt="section-icon" />
-                                        </div>
-                                        <h2 className="section__title text__white">Newsletter</h2>
-                                        <p className="section__meta">stay updated</p>
-                                    </div>
-                                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdAbjD-uXppApTFXtN5moklEphu56036NGrsGCCw8F2CUpyvA/viewform?embedded=true" width="640" height="200" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-                                    {/* <div className="newsletter-form">
-                                        <div className="form-shared">
-                                            <form action="#">
-                                                <div className="row">
-                                                    <div className="col-lg-9">
-                                                        <div className="form-group">
-                                                            <input type="email" className="form-control"
-                                                                   placeholder="Email address" />
-                                                        </div>
-                                                    </div>
-                                                    <div className="col-lg-3">
-                                                        <button className="theme-btn submit__btn"
-                                                                type="submit">subscribe
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div> */}
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="footer-top">
-                        <div className="container">
-                            <div className="row footer-widget-wrap">
-                                <div className="col footer-item footer-item1">
-                                    <h3 className="widget__title">about</h3>
-                                    <ul className="foot__links">
-                                        <li><a href="#">causes</a></li>
-                                        <li><a href="#">about</a></li>
-                                        <li><a href="#">New Campaign</a></li>
-                                        <li><a href="#">Site Map</a></li>
-                                        <li><a href="#">Events</a></li>
-                                    </ul>
-                                </div>
-                                <div className="col footer-item footer-item2">
-                                    <h3 className="widget__title">explore</h3>
-                                    <ul className="foot__links">
-                                        <li><a href="#">Press Release</a></li>
-                                        <li><a href="#">contact</a></li>
-                                        <li><a href="#">Blog Posts</a></li>
-                                        <li><a href="#">Social Connect</a></li>
-                                        <li><a href="#">Help Topics</a></li>
-                                    </ul>
-                                </div>
-                                <div className="col footer-item footer-item3">
-                                    <h3 className="widget__title">links</h3>
-                                    <ul className="foot__links">
-                                        <li><a href="#">Podcasts</a></li>
-                                        <li><a href="#">Privacy Policy</a></li>
-                                        <li><a href="#">Videos</a></li>
-                                        <li><a href="#">Terms of Use</a></li>
-                                    </ul>
-                                </div>
-                                <div className="col footer-item footer-item3">
-                                    <h3 className="widget__title">blog</h3>
-                                    <ul className="foot__links">
-                                        <li><a href="#">Care for All People</a></li>
-                                        <li><a href="#">Build school for kids</a></li>
-                                        <li><a href="#">Being Volunteer</a></li>
-                                        <li><a href="#">Raise fund to help</a></li>
-                                    </ul>
-                                </div>
-                                <div className="col footer-item footer-item4">
-                                    <h3 className="widget__title">contact</h3>
-                                    <ul className="contact__info">
-                                        <li>660 Broklyn Street, 88 New York</li>
-                                        <li><a href="mailto:needhelp@oxpitan.com">needhelp@oxpitan.com</a></li>
-                                        <li><a href="tel:6668880000">666 888 0000</a></li>
-                                    </ul>
-                                    <div className="footer__social">
-                                        <ul>
-                                            <li><a href="#"><i className="fa fa-twitter"></i></a></li>
-                                            <li><a href="#"><i className="fa fa-facebook"></i></a></li>
-                                            <li><a href="#"><i className="fa fa-pinterest"></i></a></li>
-                                            <li><a href="#"><i className="fa fa-instagram"></i></a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="footer-copyright">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <div className="copyright-desc">
-                                        <p>© Copyright 2020 by <a href="#">Layerdrops.com</a></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
-                <div onClick={this.scrollTop} id="back-to-top" className={this.state.scrollBtn ? 'back-btn-shown' : ''}>
-                    <i className="fa fa-angle-up" title="Go top"></i>
+  render() {
+    return (
+      <div>
+        <section className="footer-area p-0">
+          <div className="newsletter-area">
+            <div className="container" style={{ height: "500px" }}>
+              <div className="row h-100">
+                {/* <iframe
+                  title="Google Form for Newsletter Email Sign Up"
+                  src="https://docs.google.com/forms/d/e/1FAIpQLSdAbjD-uXppApTFXtN5moklEphu56036NGrsGCCw8F2CUpyvA/viewform?embedded=true"
+                  style={{ color: "red" }}
+                  width="640"
+                  frameborder="0"
+                  marginheight="0"
+                  marginwidth="0"
+                  style={{ height: "100%" }}
+                >
+                  Loading…
+                </iframe> */}
+                <NewsletterForm />
+              </div>
+            </div>
+          </div>
+          <div className="footer-top p-0 pt-3">
+            <div className="container">
+              <div className="row footer-widget-wrap">
+                <div className="col footer-item footer-item1">
+                  <h3 className="widget__title">Events</h3>
+                  <ul className="foot__links">
+                    <li>
+                      <a href="/events/givingtuesday">
+                        Giving Tuesday Fund Drive
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/events/2021">2021 Camp</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col footer-item footer-item2">
+                  <h3 className="widget__title">Get Involved</h3>
+                  <ul className="foot__links">
+                    <li>
+                      <a href="/donate">Donate</a>
+                    </li>
+                    <li>
+                      <a href="/volunteer">Volunteer</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col footer-item footer-item3">
+                  <h3 className="widget__title">About</h3>
+                  <ul className="foot__links">
+                    <li>
+                      <a href="/about">Mission</a>
+                    </li>
+                    <li>
+                      <a href="/team">Team</a>
+                    </li>
+                    <li>
+                      <a href="/contact">Contact</a>
+                    </li>
+                  </ul>
                 </div>
 
+                <div className="col footer-item footer-item4">
+                  <h3 className="widget__title">Contact</h3>
+                  <ul className="contact__info">
+                    <li>P.O. Box 19572 Atlanta, GA 30325</li>
+                    <li>
+                      <a href="mailto:info@pavefoundation.org">
+                        info@pavefoundation.org
+                      </a>
+                    </li>
+                    {/* <li>
+                      <a href="tel:6668880000">666 888 0000</a>
+                    </li> */}
+                  </ul>
+                  <div className="footer__social pt-2 pb-2">
+                    <ul>
+                      <li>
+                        <a href="#">
+                          <i className="fa fa-twitter"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i className="fa fa-facebook"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i className="fa fa-pinterest"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i className="fa fa-instagram"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </section>
+
+        <div
+          onClick={this.scrollTop}
+          id="back-to-top"
+          className={this.state.scrollBtn ? "back-btn-shown" : ""}
+        >
+          <i className="fa fa-angle-up" title="Go top"></i>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Footer;
---
+++ src/components/NewsletterForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function NewsletterForm() {
+  const [email, setEmail] = useState(null);
+
+  let header = {
+    emailAddress: email,
+    fvv: 1,
+    draftResponse: [null, null, "-5532202287099307631"],
+    pageHistory: 0,
+    fbzx: -5532202287099307631,
+  };
+
+  function onSubmit() {
+    axios
+      .post(
+        "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdAbjD-uXppApTFXtN5moklEphu56036NGrsGCCw8F2CUpyvA/formResponse",
+        {
+          header,
+        }
+      )
+      .then((res) => console.log("successsssssssss", res));
+  }
+
+  return (
+    <div>
+      <h1 style={{ color: "#FFF" }}>Newsletter Registration</h1>
+      <h2 style={{ color: "#FFF" }}>Email Address:</h2>
+      <input type="email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="submit" value="submit" onClick={() => onSubmit()} />
+    </div>
+  );
+}
