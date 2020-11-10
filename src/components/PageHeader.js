ec0ca1ecd726b79effcf9fecb15809096c6ba5d9
Kevin <Kevin@Kevins-MacBook-Pro.local>
2020-11-10 12:45:29 -0500

    changed up CSS

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -10,7 +10,7 @@ const PageHeader = (props) => {
     >
       <div className="container">
         <div className="row">
-          <div className="col-lg-12">
+          <div className="col-lg-12 d-flex justify-content-center">
             <div className="breadcrumb-content  mt-2 mb-2 pt-4 pb-4">
               <h2 className="breadcrumb__title" style={{ color: "#ffffff" }}>
                 {props.title}
