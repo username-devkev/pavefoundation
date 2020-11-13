d4b6a6fd7af457a4f1d814a3d72e465d495de6d9
Kevin <Kevin@Kevins-MacBook-Pro.local>
2020-11-13 15:52:13 -0500

    commiting

--- src/pages/EventsDetailPage.js
+++ src/pages/EventsDetailPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
 import NavOne from "../components/NavOne";
-import Donate from "../components/Donate";
+// import Donate from "../components/Donate";
 import PageHeader from "../components/PageHeader";
 import Footer from "../components/Footer";
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Layout from "../components/Layout";
 import NavOne from "../components/NavOne";
 import PageHeader from "../components/PageHeader";
@@ -7,23 +7,23 @@ import MixerAreaTwo from "../components/MixerAreaTwo";
 import TeamArea from "../components/TeamArea";
 import FaqArea from "../components/FaqArea";
 import ServiceArea from "../components/ServiceArea";
-import ClientsLogoTwo from "../components/ClientsLogoTwo";
+// import ClientsLogoTwo from "../components/ClientsLogoTwo";
 import Footer from "../components/Footer";
 
 const AboutPage = () => {
-    return (
-        <Layout pageTitle="Oxpitan | About">
-            <NavOne />
-            <PageHeader title="About" />
-            <AboutArea />
-            <MixerAreaTwo />
-            <TeamArea />
-            <FaqArea />
-            <ServiceArea />
-            <ClientsLogoTwo />
-            <Footer />
-        </Layout>
-    );
+  return (
+    <Layout pageTitle="Oxpitan | About">
+      <NavOne />
+      <PageHeader title="About" />
+      <AboutArea />
+      <MixerAreaTwo />
+      <TeamArea />
+      <FaqArea />
+      <ServiceArea />
+      {/* <ClientsLogoTwo /> */}
+      <Footer />
+    </Layout>
+  );
 };
 
 export default AboutPage;
